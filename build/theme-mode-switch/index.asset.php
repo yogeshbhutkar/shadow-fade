751b09893f77dc2f180db43fb9df225c3c4176f0
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-08-07 13:49:24 +0530

    fix: update theme colors

--- build/theme-mode-switch/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-compose'), 'version' => 'cc6b763066fe4e841049');
--- build/theme-mode-switch/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'e1cb8c08d88edfd3c65c', 'type' => 'module');
